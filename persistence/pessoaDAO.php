09ebdce51a167cf326e148809f7ce54ff868922c
silventino <silventinogato@gmail.com>
2018-01-25 23:40:51 -0200

    Consultar pessoa implementada, ainda nao testada.

--- persistence/pessoaDAO.php
+++ persistence/pessoaDAO.php
@@ -3,16 +3,32 @@
 
   class PessoaDAO{
     function cadastrarPessoa($pessoa, $link){
-      $SQL = "INSERT INTO Pessoa VALUES (0,
+      $SQL = "INSERT INTO Pessoa VALUES ( '".$pessoa->getEmail()."',
                                           '".$pessoa->getNickname()."',
                                           '".$pessoa->getNome()."',
-                                          '".$pessoa->getEmail()."',
                                           '".$pessoa->getSenha()."',
                                           0);";
 
   		if (!mysqli_query($link, $SQL)) {
   			die("Erro na inserção de cliente");
   		}
+    }
+
+    function consultarPessoaPorEmail($email, $link){
+
+      $SQL = "SELECT * FROM Pessoa WHERE email ='$email';";
+
+      echo $SQL."<br />";
+      $retorno = mysqli_query($link, $SQL);
+      if(!$retorno){
+        die("Erro na consulta de cliente");
+      }
+      if(mysqli_num_rows($retorno) > 0){
+        $ret = $retorno->fetch_all();
+        $resultado = new Pessoa($ret[0],$ret[1],$ret[2],$ret[3]);
+        return $resultado;
+      }
+      return null;
     }
   }
 
