0916941acbc95a2a1090f8ee7f7576ad40b0709e
silventino <silventinogato@gmail.com>
2018-01-27 14:17:34 -0200

    Descomentei uma linha importante.

--- view/visualizarJogador.php
+++ view/visualizarJogador.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-	<!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection" /> -->
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection" />
 	<link rel="stylesheet" href="./style.css" />
 </head>
 
