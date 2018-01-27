1b665579b8b4f3a64220af3e12d6be4d417a751e
KatoMono Enkeli <k4t0mono@gmail.com>
2018-01-27 01:02:20 -0200

    Listar partidas

--- persistence/partidaDAO.php
+++ persistence/partidaDAO.php
@@ -18,6 +18,24 @@
       }
     }
 
+    function listarPartidas($link) {
+      $SQL = "SELECT * FROM Partida";
+      $retorno = mysqli_query($link, $SQL);
+
+      if(!$retorno){
+        die("Erro na consulta de cliente");
+      }
+      $partidas = array();
+      if(mysqli_num_rows($retorno) > 0) {
+        $ret = $retorno->fetch_all();
+        foreach($ret as $l) {
+          $partidas[] = new Partida($l[1], $l[2], $l[3], $l[4], $l[5], $l[0]);
+        }
+        return $partidas;
+      }
+      return null;
+    }
+
   }
 
 ?>
---
+++ view/listarPartidas.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection" />
+
+	<link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+	<nav>
+		<div class="nav-wrapper">
+			<a href="#" class="brand-logo">PongSys</a>
+
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a href="../view">Home</a></li>
+				<li><a href="../view/listarJogadores.php">Jogadores</a></li>
+				<li><a href="../view/listarEquipes.php">Equipes</a></li>
+				<li><a href="../view/listarPartidas.php">Partidas</a></li>
+				<?php
+					require_once("../persistence/jogadorDAO.php");
+					require_once("../persistence/conexao.php");
+					session_start();
+					if(array_key_exists('user',$_SESSION)){
+						$conexao = new Conexao();
+						$jogadorDAO = new JogadorDAO();
+						$usuario = $jogadorDAO->consultarJogadorPorEmail($_SESSION['user'], $conexao->getLink());
+						if($usuario != null){
+							echo "<li><a>Bem-vindo, ".$usuario->getNome()."</a></li>";
+							echo "<li><a href='../control/logoff.php'>Logoff</a></li>";
+						}
+						else{
+							echo "<li><a href='login.html'>Login</a></li>";
+						}
+					}
+					else{
+						echo "<li><a href='login.html'>Login</a></li>";
+					}
+				?>
+
+			</ul>
+		</div>
+	</nav>
+
+	<div class="container center-align">
+		<h3>Partidas</h3>
+		<?php
+      require_once("../persistence/partidaDAO.php");
+			require_once("../persistence/conexao.php");
+			require_once("../persistence/equipeDAO.php");
+
+      $c = new Conexao();
+			$p = new PartidaDAO();
+			$e = new EquipeDAO();
+
+			$partidas = $p->listarPartidas($c->getLink());
+			if($partidas != null) {
+				echo "<div class='card'>";
+				echo "<table border='1' class='highlight centered'>";
+				echo "<thead><tr> <th>Id</th> <th>Equipe 1</th> <th>Equipe 2</th> <th>Data</th> <th>Estado</th> <th>Vencedores</th> </tr></thead>";
+				echo "<tbody>";
+
+				foreach($partidas as $pr) {
+					echo "<tr>";
+					echo "<td> <a href = '../view/visualizarPartida.php?id=".$pr->getIdPartida()."'></a>".$pr->getIdPartida()."</td>";
+
+					$equipe1 = $e->buscarEquipe($pr->getEquipe1(), $c->getLink())->getNome();
+					echo "<td>".$equipe1."</td>";
+
+					$equipe2 = $e->buscarEquipe($pr->getEquipe2(), $c->getLink())->getNome();
+					echo "<td>".$equipe2."</td>";
+
+					echo "<td>".$pr->getData()."</td>";
+
+					$estado = $pr->getEstado();
+					if($estado == 0) {
+						$estado = "Não realizado";
+					} elseif ($estado == 1) {
+						$estado = "Em andamento";
+					} elseif ($estado == 2) {
+						$estado = "Finalizada";
+					}
+
+					echo "<td>".$estado."</td>";
+
+					echo "<td>";
+					if($estado == "Finalizada") {
+						$result = $e->buscarEquipe($pr->getResultado(), $c->getLink())->getNome();
+						echo $result;
+					} else {
+						echo "...";
+					}
+					echo "</td>";
+
+					// echo "<tr>";
+					// echo "<td><a href = '../view/visualizarJogador.php?nick=".$jogador->getNickname()."'></a>".$jogador->getNome()."</td>";
+					// echo "<td>".$jogador->getNickname()."</td>";
+					// $equipe = $e->buscarEquipe($jogador->getIdEquipe(), $c->getLink());
+					// echo "<td>".$equipe->getNome()."</td>";
+					// echo "</tr>";
+				}
+
+				echo "</tbody>";
+				echo "</table>";
+				echo "</div>";
+
+			} else {
+				echo "<p>Não há partidas registradas</p>";
+			}
+
+			echo "<a href='./cadastroJogador.php'><button type='button' class='waves-effect waves-light btn'>Cadastrar novo jogador</button></a>";
+
+		?>
+	</div>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script type="text/javascript" src="js/tabelas.js"></script>
+</body>
+</html>
