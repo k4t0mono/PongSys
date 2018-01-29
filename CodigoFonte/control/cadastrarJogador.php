c77dd9ee7b31a35795256dae26b7167fecdb700b
silventino <silventinogato@gmail.com>
2018-01-29 15:23:28 -0200

    Tirando o controller

--- CodigoFonte/control/cadastrarJogador.php
+++ CodigoFonte/control/cadastrarJogador.php
@@ -1,17 +1,21 @@
 <?php
-
-  require_once ('../control/jogadorController.php');
-
+  require_once ('../model/jogador.php');
+  require_once ('../persistence/jogadorDAO.php');
+  require_once ('../persistence/conexao.php');
+  
   $nome = $_POST["nome"];
   $email = $_POST["email"];
   $nick = $_POST["nick"];
   $equipe = $_POST["equipe"];
   $senha = $_POST["pass"];
 
-  $jc = new JogadorController();
+  $jogador = new Jogador($email, $equipe, $nick, $nome, $senha);
 
-  $jc->cadastrarJogador($email, $equipe, $nick, $nome, $senha);
+  $conexao = new Conexao();
 
+  $jogadorDAO = new JogadorDAO();
+
+  $jogadorDAO->cadastrarJogador($jogador, $conexao->getLink());
   header('Location: ../view/listarJogadores.php');
 
 ?>
